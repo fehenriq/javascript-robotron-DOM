61d6d9e68bd36034b84ddd3fa9e64e647b53a2d7
ti7 <ti7@gimipogliano.local>
2022-09-08 11:56:37 -0300

    added new functions

--- js/main.js
+++ js/main.js
@@ -1,12 +1,80 @@
-const robotron = document.querySelector("#robotron")
-
-robotron.addEventListener("click", (evento) => {
-    console.log(evento)
-})
-
-function dizOi(nome) {
-    console.log("Oi " + nome)
-    console.log("Bem-vindo ao Robotron 2000")
+const pecas = {
+    bracos: {
+      forca: 29,
+      poder: 35,
+      energia: -21,
+      velocidade: -5,
+    },
+  
+    blindagem: {
+      forca: 41,
+      poder: 20,
+      energia: 0,
+      velocidade: -20,
+    },
+    nucleos: {
+      forca: 0,
+      poder: 7,
+      energia: 48,
+      velocidade: -24,
+    },
+    pernas: {
+      forca: 27,
+      poder: 21,
+      energia: -32,
+      velocidade: 42,
+    },
+    foguetes: {
+      forca: 0,
+      poder: 28,
+      energia: 0,
+      velocidade: -2,
+    },
+  };
+  
+const controle = document.querySelectorAll("[data-controle]");
+const estatistica = document.querySelectorAll("[data-estatistica]");
+  
+controle.forEach((elemento) => {
+    elemento.addEventListener("click", (evento) => {
+        manipulaDados(evento.target.textContent, evento.target.parentNode);
+        atualizaEstatistica(evento.target.dataset.peca, evento.target.textContent);
+    });
+});
+  
+function manipulaDados(operacao, controle) {
+    const peca = controle.querySelector("[data-contador]");
+  
+    if (operacao === "-") {
+        if (peca.value > 0) {
+            peca.value = parseInt(peca.value) - 1;
+        } else {
+            peca.value = "00";
+        }
+    } else if (operacao === "+") {
+        peca.value = parseInt(peca.value) + 1;
+    }
 }
-
-dizOi("Felipe")
+  
+function atualizaEstatistica(peca, operacao) {
+    switch (operacao) {
+        case "-":
+            subtrairEstatisticas(peca);
+        break;
+        case "+":
+            incrementarEstatisticas(peca);
+        break;
+    }
+}
+  
+function incrementarEstatisticas(peca) {
+    estatistica.forEach((elemento) => {
+        elemento.textContent = parseInt(elemento.textContent) + pecas[peca][elemento.dataset.estatistica];
+    });
+}
+  
+function subtrairEstatisticas(peca) {
+    estatistica.forEach((elemento) => {
+        elemento.textContent = parseInt(elemento.textContent) - pecas[peca][elemento.dataset.estatistica];
+    });
+}
